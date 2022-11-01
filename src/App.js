607555fba1f1d8a32a303cfaf8a0bf16daa23b35
Panchana Madara <panchanamadara16314@gmail.com>
2022-11-01 21:35:28 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
-import Header from "./components/Header";
-import styles from "./styles";
+import ApiTest from "./components/ApiTest";
+// import Header from "./components/Header";
+// import styles from "./styles";
 
 const App = () => {
   return (
-    <div
-      className={`w-full overflow-hidden ${styles.flexCenter} ${styles.paddingX} font-poppins`}>
-      <div className={`${styles.boxWidth}`}>
-        <Header />
-      </div>
+    // <div
+    //   className={`w-full overflow-hidden ${styles.flexCenter} ${styles.paddingX} font-poppins`}>
+    //   <div className={`${styles.boxWidth}`}>
+    //     <Header />
+    //   </div>
+    // </div>
+    <div>
+      <ApiTest />
     </div>
   );
 };
