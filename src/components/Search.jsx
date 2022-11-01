eb37cb7abb910e0902fec80b5d29caba611b424a
Panchana Madara <panchanamadara16314@gmail.com>
2022-11-01 21:29:20 +0530

    did some experiments

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Search = () => {
-  return <div>Search</div>;
+  const [results, setResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const getResults = async () => {
+    setIsLoading = true;
+
+    const response = await fetch(
+      "https://wft-geo-db.p.rapidapi.com/v1/geo/cities?namePrefix=col",
+      {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key":
+            "67ae22f399mshf30dea5a15dfc91p195a51jsn7e82034fddeb",
+          "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+        },
+      }
+    );
+    const data = await response.json();
+
+    setResults(data);
+    setIsLoading(false);
+  };
+
+  return (
+    <div>
+      {" "}
+      <button onClick={getResults}>
+        <span>Give ne quote!</span>
+      </button>
+    </div>
+  );
 };
 
 export default Search;
