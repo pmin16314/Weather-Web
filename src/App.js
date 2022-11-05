ce3918b262e6f3adcab3c5aa87ee2f3e3cd7ccf7
Panchana Madara <panchanamadara16314@gmail.com>
2022-11-05 18:57:34 +0530

    did css changes fore full page height

--- src/App.js
+++ src/App.js
@@ -8,14 +8,15 @@ const App = () => {
   const [locationData, setLocationData] = useState(null);
 
   return (
-    <div className={`w-full ${styles.flexCenter} ${styles.paddingX} flex-col font-poppins`}>
-      <div className={`${styles.boxWidth}`}>
+    <div
+      className={`w-full h-[100vh] max-h-[1080px] grid grid-cols-1 justify-items-center content-between sm:px-16 px-6 font-poppins`}>
+      <div className={`xl:max-w-[1600px] w-full`}>
         <Header setLocationData={setLocationData} />
       </div>
-      <div className={`${styles.boxWidth}`}>
+      <div className={`xl:max-w-[1600px] w-full`}>
         {locationData && <WeatherR locationData={locationData} />}
       </div>
-      <div className="mt-6 text-miniText font-medium">
+      <div className="mt-6 mb-6 text-miniText font-medium">
         Copyright Ⓒ 2022 The weather Report. All Rights Reserved.
       </div>
     </div>
